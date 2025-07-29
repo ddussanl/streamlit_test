9499ccc8cc226de3adf6e0202ffee890017e428a
ddussanl <ddussan85@gmail.com>
2025-07-29 09:36:36 -0500

    se agrega control deslizante y boton al programa

--- app.py
+++ app.py
@@ -2,5 +2,13 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
+number_of_trials = st.slider('Numero de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar!')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    
+                            
+
 st.write('Esta aplicacion aun no es funcional. En construccion')
 
